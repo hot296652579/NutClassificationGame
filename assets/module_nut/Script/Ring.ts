74cbbd1c6729dfa57462adf8cd5bb9f2a6721e2c
Super_Javan <296652579@qq.com>
2024-12-09 15:00:45 +0800

    悬浮效果

--- assets/module_nut/Script/Ring.ts
+++ assets/module_nut/Script/Ring.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, tween, Vec3 } from 'cc';
+import { _decorator, Component, Node, Quat, tween, Vec3 } from 'cc';
 import { ScrewColor } from './Enum/ScrewColor';
 import { duration } from './NutComponent';
 
@@ -11,62 +11,68 @@ export class Ring extends Component {
     color: ScrewColor = ScrewColor.RED; //对应各自的颜色枚举
 
     @property
-    hoverHeight: number = 0.2; // 悬浮高度（上下浮动的幅度）
+    hoverHeight: number = 0.3; // 悬浮高度（上下浮动的幅度）
 
     @property
-    hoverWidth: number = 0.2; // 悬浮宽度（左右浮动的幅度）
+    tiltAngle: number = 2; // 左右倾斜的角度（度数）
 
     @property
     duration: number = 2; // 单次浮动的时间（秒）
 
-    private originPosition: Vec3 = new Vec3(); // 初始位置
-
+    private originalPosition: Vec3 = new Vec3(); // 初始位置
+    private originalRotation: Quat = new Quat(); // 保存初始旋转
 
     onLoad() {
     }
 
     //悬浮效果
     suspensionEffect() {
-        this.originPosition = this.node.position;
-
+        const pos = this.node.position;
+        const rotation = this.node.rotation;
         // 定义上下浮动的目标位置
         const upPosition = new Vec3(
-            this.node.position.x,
-            this.node.position.y + this.hoverHeight,
-            this.node.position.z
+            pos.x,
+            pos.y + this.hoverHeight,
+            pos.z
         );
         const downPosition = new Vec3(
-            this.node.position.x,
-            this.node.position.y - this.hoverHeight,
-            this.node.position.z
+            pos.x,
+            pos.y - this.hoverHeight,
+            pos.z
         );
 
-        // 定义左右浮动的目标位置
-        const leftPosition = new Vec3(
-            this.node.position.x - this.hoverWidth,
-            this.node.position.y,
-            this.node.position.z
+        // 定义左右倾斜的目标旋转
+        const leftTilt = Quat.fromEuler(
+            new Quat(),
+            rotation.x,
+            rotation.y,
+            rotation.z - this.tiltAngle
         );
-        const rightPosition = new Vec3(
-            this.node.position.x + this.hoverWidth,
-            this.node.position.y,
-            this.node.position.z
+        const rightTilt = Quat.fromEuler(
+            new Quat(),
+            rotation.x,
+            rotation.y,
+            rotation.z + this.tiltAngle
         );
 
-        // 使用 tween 实现上下和左右的浮动
+        // 使用 tween 实现上下浮动和左右倾斜的组合效果
         tween(this.node)
-            .to(this.duration, { position: upPosition }, { easing: 'sineInOut' })
-            .to(this.duration, { position: downPosition }, { easing: 'sineInOut' })
-            .to(this.duration, { position: rightPosition }, { easing: 'sineInOut' })
-            .to(this.duration, { position: leftPosition }, { easing: 'sineInOut' })
+            .parallel( // 并行执行上下浮动和旋转动画
+                tween()
+                    .to(this.duration, { position: upPosition }, { easing: 'sineInOut' })
+                    .to(this.duration, { position: downPosition }, { easing: 'sineInOut' }),
+                tween()
+                    .to(this.duration, { rotation: leftTilt }, { easing: 'sineInOut' })
+                    .to(this.duration, { rotation: rightTilt }, { easing: 'sineInOut' })
+            )
             .union() // 合并动画序列
             .repeatForever() // 无限循环
             .start();
     }
-
     stopHover() {
         tween(this.node).stop();
-        this.node.setPosition(this.originPosition);
+        this.node.setPosition(this.originalPosition);
+        this.node.setRotation(this.originalRotation);
     }
 }
 
