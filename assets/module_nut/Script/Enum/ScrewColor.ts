6987ccbfda075e0e87aa79130f4969f97cd78c4c
Super_Javan <296652579@qq.com>
2024-12-12 10:07:46 +0800

    修改枚举

--- assets/module_nut/Script/Enum/ScrewColor.ts
+++ assets/module_nut/Script/Enum/ScrewColor.ts
@@ -1,12 +1,12 @@
 export enum ScrewColor {
     Unknow = 0, //未知
     EMPTY = -1, //空
-    RED = 1,    //红色
+    PINK = 1,    //粉色
     BLUE = 2,   //蓝色
     YELLOW = 3, //黄色
     GREEN = 4,  //绿色
     ORANGE = 5, //橙色
-    CYAN = 6,    //青色
+    WHITE = 6,    //白色
     PURPLE = 7, //紫色
-    PURPLE_RED = 8, //紫红色
+    BLACK = 8, //黑色
 }
