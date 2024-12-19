43f13fcc9a898522e39da7e84e20ab0ba855e340
PS2022SPZEUPMN\Admin <910647682@qq.com>
2024-12-19 17:15:49 +0800

    关卡更新

--- assets/module_nut/Script/Enum/ScrewColor.ts
+++ assets/module_nut/Script/Enum/ScrewColor.ts
@@ -9,8 +9,8 @@ export enum ScrewColor {
     WHITE = 6,    //白色
     PURPLE = 7, //紫色
     BLACK = 8, //黑色
-    Palepink = 9, //浅粉
-    Red = 10, //红色
+    PALEPINK = 9, //浅粉
+    RED = 10, //红色
 }
 
 export const HexScrewColor = {
@@ -22,6 +22,6 @@ export const HexScrewColor = {
     [ScrewColor.WHITE]: '#C1B2D1',    //白色
     [ScrewColor.PURPLE]: '#921397', //紫色
     [ScrewColor.BLACK]: '#323135', //黑色
-    [ScrewColor.Palepink]: '#EB9682', //浅粉
-    [ScrewColor.Red]: '#F03636', //红色
+    [ScrewColor.PALEPINK]: '#EB9682', //浅粉
+    [ScrewColor.RED]: '#F03636', //红色
 }
